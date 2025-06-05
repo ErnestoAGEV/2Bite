f996113ebd1f955c766b9253d7a526c6f1ac8710
ErnestoAGEV <vevelerne@gmail.com>
2025-06-04 19:31:52 -0600

    Implemntacion de componentes y creacion de registro

--- src/Presentation/views/register/Register.tsx
+++ src/Presentation/views/register/Register.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import {
   Image,
-  StyleSheet,
   Text,
-  TextInput,
-  ToastAndroid,
   View,
 } from "react-native";
 import { RoundedButton } from "../../components/RoundedButton";
